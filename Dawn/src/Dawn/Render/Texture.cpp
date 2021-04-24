937c58d15851fdcb6c1852d2bc389a4e4f2579ea
ben-humphries <benfhumphries@gmail.com>
2021-04-24 10:30:01 -0400

    Made texture filtering nearest neighbor for now

--- Dawn/src/Dawn/Render/Texture.cpp
+++ Dawn/src/Dawn/Render/Texture.cpp
@@ -15,6 +15,9 @@ namespace Dawn
         glGenTextures(1, &m_textureHandle);
         bind();
 
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.getWidth(), image.getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, image.getDataPtr());
         glGenerateMipmap(GL_TEXTURE_2D);
 
